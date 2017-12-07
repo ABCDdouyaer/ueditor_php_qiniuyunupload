64bde3708965a2fe2b9e97ce58e0ce049fb4f610
wangwei <973864658@qq.com>
2017-12-07 23:27:44 +0800

    haha
    Signed-off-by: wangwei <973864658@qq.com>

--- php/conf.php
+++ php/conf.php
@@ -11,8 +11,8 @@ $QINIU_UP_HOST	= 'http://up.qiniu.com';
 $QINIU_RS_HOST	= 'http://rs.qbox.me';
 $QINIU_RSF_HOST	= 'http://rsf.qbox.me';
 
-$QINIU_ACCESS_KEY	= 'KUN6xYZlOAtid2MjHm90-6VFY2M7HC90ijDH4uOR';
-$QINIU_SECRET_KEY	= 'D-K57TE5hPe3krexftxLWFKmL2xbQEKA-mtkrUfB';
+$QINIU_ACCESS_KEY	= 'ynk7jl9jenFIsiVmr6MebLRMUF7fxds-li6MzGvc';
+$QINIU_SECRET_KEY	= 'ciL5oC6OaofU8ucflaJhGd5ATLg4yGl8pzh5H4sY';
 
-$BUCKET = "gitwiduu";
+$BUCKET = "blog";
 
--- ueditor.all.js
+++ ueditor.all.js
@@ -17923,7 +17923,7 @@ UE.plugins['video'] = function (){
                     html.push('</tr>')
                 }
                 //禁止指定table-width
-                return '<table><tbody>' + html.join('') + '</tbody></table>'
+                return '<table border="1 solid black" cellpadding="0" cellspacing="0"><tbody>' + html.join('') + '</tbody></table>'
             }
 
             if (!opt) {
